88eb1295e9da6ee0ec0aeff1de3ad3cbf9adc995
Blacklion567 <113174497+Blacklion567@users.noreply.github.com>
2023-08-24 19:08:53 +0800

    Little-Projects-HTML-CSS-JS
    'Love God; love others; do your best in all you do, even if you fall short!' - Bethany Hamilton
    
    'Always Do Your Best. Your best is going to change from moment to moment; it will be different when you are healthy as opposed to sick. Under any circumstance, simply do your best, and you will avoid self-judgment, self-abuse and regret.' - Don Miguel Ruiz
    
    'Leap, and the net will appear.' - John Burroughs
    
    'Do your best when no one is looking. If you do that, then you can be successful at anything you put your mind to' - Bob Cousy
    
    'The difference between ordinary and extraordinary is that little difference' - Jimmy Johnson

---
+++ 100-hulu webpage clone/script.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector('.modal');
+const loginButton = document.querySelector('.login-btn');
+const closeButton = document.querySelector('.close');
+
+const openModal = () => (modal.style.display = 'block');
+const closeModal = () => (modal.style.display = 'none');
+
+const outsideClick = () => {
+  if (e.target == modal) {
+    closeModal();
+  }
+};
+
+loginButton.addEventListener('click', openModal);
+closeButton.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
